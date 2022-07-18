8ed3c9a58514f1c4895a91f31ac639b0e5b54ddc
Jason Luera <jasonluera@gmail.com>
2022-07-18 18:27:09 +0000

    Changed dag to run once instead of daily

--- airflow/dags/etl_finance_subreddit_dag.py
+++ airflow/dags/etl_finance_subreddit_dag.py
@@ -13,7 +13,7 @@ output_name = datetime.now().strftime("%Y%m%d")
 
 # Run our DAG daily and ensures DAG run will kick off 
 # once Airflow is started, as it will try to "catch up"
-schedule_interval = '@daily' 
+schedule_interval = '@once' 
 start_date = days_ago(1)
 
 default_args = {"owner": "airflow", "depends_on_past": False, "retries": 1}
